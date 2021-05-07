0af7d872b23254646ee0d1ca257c77006eaee58b
Carl Reyes <reyes.carlj@gmail.com>
2021-05-07 17:52:32 -0400

    Update babel config
    We need to include the babel runtime regenerator in order to use async
    functions (i.e. in our action creators)
    [reference](https://github.com/babel/babel/issues/5085)

--- src/actions/Launches.js
+++ src/actions/Launches.js
@@ -1,27 +1,31 @@
-import LaunchService from '../services/LaunchService';
+import LaunchService from "../services/LaunchService";
 
 export const ACTIONS = {
-  REQUEST_LAUNCHES: 'REQUEST_LAUNCHES',
-  RECEIVE_LAUNCHES: 'RECEIVE_LAUNCHES'
+  REQUEST_LAUNCHES: "REQUEST_LAUNCHES",
+  RECEIVE_LAUNCHES: "RECEIVE_LAUNCHES",
 };
 
 export const requestLaunches = () => ({
-  type: ACTIONS.REQUEST_LAUNCHES
+  type: ACTIONS.REQUEST_LAUNCHES,
 });
 
-const receiveLaunches = response => ({
+const receiveLaunches = (response) => ({
   type: ACTIONS.RECEIVE_LAUNCHES,
   payload: {
-    launches: response.data
-  }
+    launches: response.data,
+  },
 });
 
-export const fetchLaunches = dispatch => {
+export const fetchLaunches = async (dispatch) => {
   dispatch(requestLaunches());
-  return LaunchService.get().then(response => dispatch(receiveLaunches(response)));
+
+  const response = await LaunchService.get();
+
+  dispatch(receiveLaunches(response));
 };
 
-const shouldFetchLaunches = launchCollection => !launchCollection || !launchCollection.fetching;
+const shouldFetchLaunches = (launchCollection) =>
+  !launchCollection || !launchCollection.fetching;
 
 export const fetchLaunchesIfNeeded = ({ dispatch, launchCollection }) =>
   shouldFetchLaunches(launchCollection) && fetchLaunches(dispatch);
