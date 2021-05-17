5c91f963e51c201734a72f516553489d89b73ab8
Carl Reyes <reyes.carlj@gmail.com>
2021-05-17 17:41:10 -0400

    wip

--- src/components/Launch.jsx
+++ src/components/Launch.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
-const Launch = ({ launch }) => (
-  <li>
-    <h2> {launch.mission_name} </h2>
-    <div> Flight Number: {launch.flight_number} </div>
-  </li>
+const Launch = ({ launch, launchId, currentlySelected, setSelectedLaunch }) => (
+  <div
+    tabIndex="0"
+    role="button"
+    onClick={() => setSelectedLaunch(launch)}
+    onKeyDown={() => setSelectedLaunch(launch)}
+  >
+    <details>
+      <summary>
+        Mission: {launch.mission_name} - Flight Number: {launch.flight_number}
+      </summary>
+      <div className={currentlySelected && "expanded"}>
+        <p>Rocket ID: {launch.rocket.rocket_id}</p>
+        <p>Description: {launch.details}</p>
+      </div>
+    </details>
+  </div>
 );
 
 export default Launch;
--- src/views/Launches.jsx
+++ src/views/Launches.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchLaunches } from "../actions/Launches";
 import Layout from "./Layout";
@@ -7,12 +7,18 @@ import Navigation from "../components/Navigation";
 
 const menu = Navigation();
 
+const generateLaunchId = (flightNumber, missionName) =>
+  `${flightNumber}+${missionName}`;
+
 const Launches = () => {
+  const [selectedLaunch, setSelectedLaunch] = useState(null);
   const { launches, fetching } = useSelector((state) => state.launchCollection);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchLaunches);
+    if (!launches.length) {
+      dispatch(fetchLaunches);
+    }
   }, []);
 
   return (
@@ -21,9 +27,21 @@ const Launches = () => {
         <div>LOADING</div>
       ) : (
         <div>
-          {launches.map((launch) => (
-            <Launch key={launch.launch_id} launch={launch} />
-          ))}
+          {launches.map((launch) => {
+            const launchId = generateLaunchId(
+              launch.flight_number,
+              launch.mission_name
+            );
+            return (
+              <Launch
+                key={launchId}
+                launchId={launchId}
+                launch={launch}
+                currentlySelected={launchId === selectedLaunch}
+                setSelectedLaunch={setSelectedLaunch}
+              />
+            );
+          })}
         </div>
       )}
     </Layout>
